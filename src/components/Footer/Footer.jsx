5017ceaabd911891f216d24316df55005647f531
mamun-prodhan <mamunpr9@gmail.com>
2023-09-08 18:51:44 +0600

    footer responsive completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       item3: "How it works",
     },
     {
-      item1: "Complaints about a member ",
+      item1: "Complaints",
       item2: "Secure contacts",
       item3: "Terms of use",
     },
@@ -29,7 +29,7 @@ const Footer = () => {
   ];
 
   return (
-    <div className=" bg-[#29273E] pt-56 pb-28 px-24 relative">
+    <div className=" bg-[#29273E] pt-56 pb-10 lg:pb-28 px-3 md:px-8 lg:px-24 relative">
       {/*  */}
       <div className="container m-auto bg-[#5248AA] py-6 md:py-10 lg:py-12 px-3 md:px-6 lg:px-36 rounded-[10px] flex flex-col lg:flex-row-reverse items-center gap-5 lg:gap-40 absolute top-[-1%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
         <div className="flex-1">
@@ -49,12 +49,12 @@ const Footer = () => {
         </div>
       </div>
       {/*  */}
-      <div className="flex flex-col md:flex-row items-center justify-between text-lg text-[#FFF] mb-28">
+      <div className="flex gap-5 md:gap-0 flex-col md:flex-row items-center justify-between text-base md:text-lg text-center md:text-start text-[#FFF] mb-12 mg:mb-16 lg:mb-28">
         {datas.map((data, index) => (
           <FooterItem data={data} key={index}></FooterItem>
         ))}
       </div>
-      <div className="flex gap-9 mb-8">
+      <div className="flex gap-9 mb-8 justify-center md:justify-normal">
         <p className="text-lg font-bold text-[#FFF]">Follow us</p>
         <div className="flex gap-4">
           <img src={facebook} alt="facebook" />
